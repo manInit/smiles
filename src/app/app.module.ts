15bc5a033e1e831f8b180d2a797fee4ba776eeef
manInit <info.denisbronnikov@gmail.com>
2021-08-19 21:50:24 +0900

    add fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageAllComponent } from './page-all/page-all.component';
+import { PageAllService } from "./page-all/page-all.service";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAllComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [PageAllService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
