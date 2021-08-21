01e9999539a38f224b1dd20c28942b6f4767ac74
manInit <info.denisbronnikov@gmail.com>
2021-08-22 02:06:25 +0900

    add icon types and remove star from love

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar, IconDefinition } from '@fortawesome/free-regular-svg-icons';
 import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Emoji } from '../interfaces/emoji';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-table',
@@ -16,11 +17,12 @@ export class TableComponent implements OnInit {
   @Input() loveHandler!: Function
   @Input() restoreHandler!: Function
 
-  faStar = faStar
-  fasStar = fasStar
-  faTimes = faTimes
+  isShowFavorite: boolean = !(this.router.url === '/love')
+  faStar: IconDefinition = faStar
+  fasStar: IconDefinition = fasStar
+  faTimes: IconDefinition = faTimes
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void { }
 }
