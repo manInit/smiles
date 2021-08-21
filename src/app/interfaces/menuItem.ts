f2ea52c7726314ab2553ff4d077a60c26d18ed4c
manInit <info.denisbronnikov@gmail.com>
2021-08-22 01:58:16 +0900

    add menu items and function set active menu item

---
+++ src/app/interfaces/menuItem.ts
@@ -0,0 +1,5 @@
+export interface MenuItem {
+  title: string
+  isActive: boolean
+  link: string
+}
---
+++ src/app/menuItems.ts
@@ -0,0 +1,26 @@
+import { MenuItem } from './interfaces/menuItem';
+
+export const menuItems: MenuItem[] = [
+  {
+    title: 'Все',
+    isActive: false,
+    link: '/'
+  },
+  {
+    title: 'Любимые',
+    isActive: false,
+    link: '/love'
+  },
+  {
+    title: 'Удаленные',
+    isActive: false,
+    link: '/deleted'
+  }
+];
+
+export function setActive(name: string) {
+  menuItems.forEach(item => item.isActive = false);
+
+  const activeItem = menuItems.find(item => item.title === name);
+  if (activeItem) activeItem.isActive = true;
+}
