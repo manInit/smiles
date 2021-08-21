ea840cfee01e2a7d30ddf240e40bfbed91af1e3f
manInit <info.denisbronnikov@gmail.com>
2021-08-22 01:58:29 +0900

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageLoveComponent } from './page-love/page-love.component';
+import { PageAllComponent } from './page-all/page-all.component';
+import { PageDeletedComponent } from './page-deleted/page-deleted.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PageAllComponent },
+  { path: 'love', component: PageLoveComponent },
+  { path: 'deleted', component: PageDeletedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
