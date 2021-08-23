da463ad8fa5f1be3900d0278990e73e0d779abe1
manInit <info.denisbronnikov@gmail.com>
2021-08-23 20:42:49 +0900

    full size img can show on top

--- src/app/hover-img/hover-img.component.ts
+++ src/app/hover-img/hover-img.component.ts
@@ -13,9 +13,22 @@ export class HoverImgComponent implements OnInit {
   ngOnInit(): void { }
 
   onHover($event: any) {
-    const preview = $event.target;
-    const fullSize = preview.nextElementSibling;
-    fullSize.style.top = preview.getBoundingClientRect().bottom + 'px';
-    fullSize.style.left = preview.getBoundingClientRect().right + 'px';
+    const preview: HTMLElement = $event.target;
+    const fullSize: Element | null = preview.nextElementSibling;
+    if (!fullSize || !(fullSize instanceof HTMLElement)) {
+      throw new Error('Не найден элемент с полноразмерной картинкой');
+    }
+
+    const bottomCoord: number = preview.getBoundingClientRect().bottom;
+    if (bottomCoord + fullSize.offsetHeight < window.innerHeight * 0.75) {
+      fullSize.style.top = `${bottomCoord}px`;
+    } else {
+      const topCoord: number = preview.getBoundingClientRect().y - 
+                               fullSize.offsetHeight -
+                               preview.offsetHeight;
+      fullSize.style.top = `${topCoord}px`;
+    }
+
+    fullSize.style.left = `${preview.getBoundingClientRect().right}px`;
   }
 }
